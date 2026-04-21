ab23bd46dfd45359fc9fa6263c29ec126face87e
chrislong369 <chris.long@hotmail.com>
2026-04-21 09:28:59 -0400

    Improve organizer launcher folder detection and path output

--- run_organizer.bat
+++ run_organizer.bat
@@ -1,14 +1,24 @@
 @echo off
 SETLOCAL
 
-REM Default drop folder on Desktop
+REM Preferred drop folder names on Desktop
 SET DROP_FOLDER=%USERPROFILE%\Desktop\3D_DROP
+SET ALT_DROP_FOLDER=%USERPROFILE%\Desktop\3-D_DROP
+SET ALT_DROP_FOLDER2=%USERPROFILE%\Desktop\3-D drop
+SET ALT_DROP_FOLDER3=%USERPROFILE%\Desktop\3D drop
+
+IF EXIST "%ALT_DROP_FOLDER%" SET DROP_FOLDER=%ALT_DROP_FOLDER%
+IF EXIST "%ALT_DROP_FOLDER2%" SET DROP_FOLDER=%ALT_DROP_FOLDER2%
+IF EXIST "%ALT_DROP_FOLDER3%" SET DROP_FOLDER=%ALT_DROP_FOLDER3%
 
 ECHO -----------------------------------------
 ECHO 3D FILE ORGANIZER - ONE CLICK RUN
 ECHO -----------------------------------------
+ECHO Using drop folder:
+ECHO %DROP_FOLDER%
 
 IF NOT EXIST "%DROP_FOLDER%" (
+    ECHO.
     ECHO Creating drop folder on Desktop...
     mkdir "%DROP_FOLDER%"
     ECHO.
